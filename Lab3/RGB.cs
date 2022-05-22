e7396e210f6c4f732b6d1eb2d0563b1576b8169d
Mateusz Wszola <matwsz000@utp.edu.pl>
2022-05-22 21:22:27 +0200

    Added project files

---
+++ Lab3/RGB.cs
@@ -0,0 +1,15 @@
+namespace Lab3;
+
+public class RGB
+{
+    private int R_value { get; set; }
+    private int G_value { get; set; }
+    private int B_value { get; set; }
+
+    public RGB(int rValue, int gValue, int bValue)
+    {
+        R_value = rValue;
+        G_value = gValue;
+        B_value = bValue;
+    }
+}
