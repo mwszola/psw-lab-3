728c1da6ad45f613285b76713be93f7a1a22b59c
Mateusz Wszola <matwsz000@utp.edu.pl>
2022-05-29 16:18:17 +0200

    Added solution to Task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -5,11 +5,64 @@ public static class Program
     public static void Main()
     {
         var uni = new Uni();
-        var student = uni.DodajStudenta("John", "Doe", 1);
-        student.DisplayStudent();
+        var isRunning = true;
 
-        Console.WriteLine("Podaj nr indeksu studenta:");
-        var nrIndeksu = int.Parse(Console.ReadLine() ?? "-1");
-        uni.UsunStudenta(nrIndeksu);
+        while (isRunning)
+        {
+            Console.WriteLine("Prosze, podaj akcje:");
+            Console.WriteLine("1) Dodaj studenta");
+            Console.WriteLine("2) Usun studenta z listy");
+            Console.WriteLine("3) Oblicz srednia ocen studenta");
+            Console.WriteLine("4) Oblicz srednia wszystkich studentow");
+            Console.WriteLine("5) Zakoncz program");
+
+            var action = Console.ReadLine();
+
+            switch (action)
+            {
+                case "1":
+                    try
+                    {
+                        Console.WriteLine("Dodawanie studenta..");
+                        Console.WriteLine("Podaj imie:");
+                        var imie = Console.ReadLine() ?? "N/A";
+                        Console.WriteLine("Podaj nazwisko:");
+                        var nazwisko = Console.ReadLine() ?? "N/A";
+                        Console.WriteLine("Podaj rok studiow:");
+                        var rokSt = int.Parse(Console.ReadLine() ?? "0");
+                        var student = uni.DodajStudenta(imie, nazwisko, rokSt);
+                        Console.WriteLine("Dodano studenta...");
+                        student.DisplayStudent();
+                    }
+                    catch (ApplicationException error)
+                    {
+                        Console.WriteLine(error.Message);
+                    }
+
+                    break;
+                case "2":
+                    Console.WriteLine("Usuwanie studenta z listy...");
+                    Console.WriteLine("Prosze, podaj numer indeksu");
+                    uni.UsunStudenta(int.Parse(Console.ReadLine()));
+                    break;
+                case "3":
+                    Console.WriteLine("Obliczanie sredniej ocen studenta...");
+                    Console.WriteLine("Prosze, podaj nr indeksu studenta");
+                    var st_3 = uni.ZnajdzStudenta(int.Parse(Console.ReadLine() ?? "-1"));
+                    if (st_3 != null)
+                    {
+                        Console.WriteLine($"Srednia wynosi {uni.ObliczSrednia(st_3)}");
+                    }
+                    break;
+                case "4":
+                    Console.WriteLine("Obliczanie sredniej ocen wszystkich studentow");
+                    Console.WriteLine($"Srednia wynosi: {uni.ObliczSredniaAll()}");
+                    break;
+                case "5":
+                    Console.WriteLine("Zakonczenie programu...");
+                    isRunning = false;
+                    break;
+            }
+        }
     }
 }
--- Task2/Uni.cs
+++ Task2/Uni.cs
@@ -2,41 +2,85 @@ namespace Task2;
 
 public class Uni
 {
-    public double[] ListaDopuszczalnychOcen = {2, 3, 3.5, 4, 4.5, 5};
+    private readonly double[] ListaDopuszczalnychOcen = {2, 3, 3.5, 4, 4.5, 5};
     private readonly List<Student> ListaStudentow = new();
 
     public Student DodajStudenta(string imie, string nazwisko, int rokSt)
     {
-        // 1. Utworz obiekt "Student"
+        // Utworzenie obiektu klasy "Student"
         var nrIndeksu = ListaStudentow.Count + 1;
         var student = new Student(nrIndeksu, imie, nazwisko, rokSt);
-        
-        // 2. Pobierz oceny studenta z klawiatury
+
+        // Wprowadzenie ocen studenta z klawiatury
         Console.WriteLine("Prosze, podaj oceny studenta oddzielone przecinkiem:");
         var input = Console.ReadLine()?.Split(",") ?? Array.Empty<string>();
 
         // Konwersja ocen
-        student.Oceny = input.Select(double.Parse).ToList();
-        
-        // 3. Dodaj studenta do "ListaStudentow"
+        var oceny = input.Select(double.Parse).ToList();
+
+        // Porownanie wprowadzonych ocen z lista dopuszczalnych ocen
+        if (oceny.Any(ocena => !ListaDopuszczalnychOcen.Contains(ocena)))
+        {
+            throw new ApplicationException(
+                $"Dopuszczalne oceny to: {ListaDopuszczalnychOcen.Aggregate("", (current, ocena) => current + (" " + ocena))}");
+        }
+        // Przypisanie ocen studentowi
+        student.Oceny = oceny;
+        // Dodanie studenta do "ListaStudentow"
         ListaStudentow.Add(student);
-        
+
         return student;
+    }
+
+    public Student? ZnajdzStudenta(int nr_indeksu)
+    {
+        var st = ListaStudentow.Find(st => st.Nr_indeksu == nr_indeksu);
+        if (st == null)
+        {
+            Console.WriteLine("Nie znaleziono studenta");    
+        }
+
+        return st;
     }
 
     public Student? UsunStudenta(int nr_indeksu)
     {
-        var st = ListaStudentow.Find(st => st.Nr_indeksu == nr_indeksu);  
+        var st = ZnajdzStudenta(nr_indeksu);
         if (st != null)
         {
             ListaStudentow.Remove(st);
             Console.WriteLine($"Student o numerze indeksu: {nr_indeksu}, zostal usuniety z listy");
         }
-        else
+        
+        return st;
+    }
+
+    public double ObliczSrednia(Student student)
+    {
+        double srednia = 0;
+        if (student.Oceny.Count > 0)
         {
-            Console.WriteLine($"Student o numerze indeksu: {nr_indeksu}, nie zostal znaleziony");
+            srednia = student.Oceny.Sum() / student.Oceny.Count;
         }
 
-        return st;
-    } 
+        return srednia;
+    }
+
+    public double ObliczSredniaAll()
+    {
+        double sumaOcen = 0;
+        double liczbaOcen = 0;
+        if (ListaStudentow.Count <= 0)
+        {
+            return 0;
+        }
+        
+        foreach (var student in ListaStudentow)
+        {
+            sumaOcen += student.Oceny.Sum();
+            liczbaOcen += student.Oceny.Count;
+        }
+
+        return sumaOcen / liczbaOcen;
+    }
 }
